57f2cac8003f7ea1c564195b82e770f143a5caa1
Andres Ortega <AndiO@AndiOs-MacBook.local>
2017-01-19 19:59:32 -0500

    I added file

---
+++ navbarBBM copy.php
@@ -0,0 +1,31 @@
+<style>
+
+.navbar{
+  background-color: #000;
+}
+.navbar-nav li.active a {
+  background-color: #000!important;
+  }
+
+.white{
+  color: #fff!important;
+}
+
+</style>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Group/Name</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="" data-toggle="modal" data-target="#myModalsettings"><span class ="glyphicons glyphicons-settings white"></span></a></li>
+      </ul>
+    </div>
+  </div>
+</
+  
+
+</body>
+
